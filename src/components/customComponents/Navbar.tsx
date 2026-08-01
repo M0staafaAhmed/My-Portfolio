4c5dc65373a9d8b4dd26f5bbde4d332c11053474
Mostafa Ahmed <your_email@example.com>
2026-08-01 04:02:50 +0300

    my portfolio

--- src/components/customComponents/Navbar.tsx
+++ src/components/customComponents/Navbar.tsx
@@ -25,8 +25,8 @@ export default function Navbar() {
     }
     return (
         <>
-            <div className="fixed inset-0 z-51 h-fit pt-4 px-3">
-                <nav className="max-w-4xl mx-auto mt-6 flex items-center justify-between px-6 py-3 rounded-4xl border border-primary/30 bg-neutral/5 backdrop-blur-xl shadow-[0_0_25px_-5px_var(--color-primary)]">
+            <div className="fixed inset-0 z-51 h-fit pt-4 px-3 max-w-4xl mx-auto">
+                <nav className="w-full mx-auto mt-6 flex items-center justify-between px-6 py-3 rounded-4xl border border-primary/30 bg-neutral/5 backdrop-blur-xl shadow-[0_0_25px_-5px_var(--color-primary)]">
 
                     {/* Logo */}
                     <a href="#hero" className="text-lg font-bold tracking-tight text-white" data-cursor="hover" data-cursor-color={"#fff"}>
@@ -57,7 +57,7 @@ export default function Navbar() {
                     <MagneticButton
                         data-cursor-color={"#dc143c"}
                         data-cursor="hover"
-                        className="hidden! md:block px-5 py-2 text-sm font-semibold text-white rounded-4xl border border-primary bg-primary/10 transition-all duration-300"
+                        className="hidden! md:block! px-5 py-2 text-sm font-semibold text-white rounded-4xl border border-primary bg-primary/10 transition-all duration-300"
                         title="dowload Cv"
                         onClick={handleDownloadCv}
                     >
